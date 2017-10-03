f545113818ea348d38b12f7f2e9565d611db8a2b
Romain Martinez <martinez.staps@gmail.com>
2017-10-03 10:40:34 -0400

    try to optimize numpy array to list [not working]

--- mvc/fileio.py
+++ mvc/fileio.py
@@ -1,8 +1,9 @@
 import os
 import scipy.io as sio
+import numpy as np  # todo: delete this
 
 
-class Fileio:
+class ImportMat:
     def __init__(self, datapath):
         if not os.path.isdir(datapath):
             raise ValueError('please provide a valid data path')
@@ -30,5 +31,18 @@ class Fileio:
 
 if __name__ == '__main__':
     path2data = '/media/romain/E/Projet_MVC/data/Final_output'
-    dummy = Fileio(path2data)
+    dummy = ImportMat(path2data)
+    print()
+
+    for idataset, matrix in enumerate(dummy.data.values()):
+        # preallocate
+        participants, datasets, muscles, tests, relative_mvc = (np.array([], dtype='int') for i in range(5))
+        absolute_mvc = np.array([], dtype='float')
+        for (iparticipant, imuscle, itest), mvc in np.ndenumerate(matrix):
+            participants = np.append(participants, iparticipant + 1)
+            datasets = np.append(datasets, idataset + 1)
+            muscles = np.append(muscles, imuscle + 1)
+            tests = np.append(tests, itest + 1)
+            absolute_mvc = np.append(absolute_mvc, mvc)
+        print()
     print()
